c39a3b6f7ae2f1156baf12ee88b56c3a19cb74e0
Tangxing Zhou <zhoutangxing@126.com>
2018-04-22 21:30:57 +0800

    start the Baike project

---
+++ src/main/java/com/ztx/baike/App.java
@@ -0,0 +1,13 @@
+package com.ztx.baike;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
